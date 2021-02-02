6f61710dcba1a8fc68975fc2dcb3f004c7b55955
Word Press <wordpress@ip-172-31-41-206.us-west-2.compute.internal>
2021-02-02 04:32:21 +0000

    Fix one code issue
    Signed-off-by: Word Press <wordpress@ip-172-31-41-206.us-west-2.compute.internal>

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <!doctype html>
 <html lang="<?php language_attributes();?>">
 <head>
-    <meta charset="<?php bloginfo( 'charset' )">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -17,7 +17,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class( 'hello-class' ); ?>>
 
 <?php wp_body_open(); ?>
 
