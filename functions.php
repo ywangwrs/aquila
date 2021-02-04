539660e251672c7265f0a7985b8a4be9817faf4c
Yang Wang <ywangwrs@gmail.com>
2021-02-03 23:13:09 -0500

    Until lecture #6

--- functions.php
+++ functions.php
@@ -4,4 +4,14 @@
  *
  * @package Aquila
  */
+#echo '<pre>';
+#print_r( filemtime( get_template_directory() . '/style.css') );
+#wp_die();
+
+function aquila_enqueue_scripts() {
+    wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all' );
+}
+
+add_action ( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' );
+
 ?>
