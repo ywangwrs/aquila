918b85efc216ea2863f8ed0c1bd74ca813af8f69
Yang Wang <ywangwrs@gmail.com>
2021-02-03 23:58:27 -0500

    Until lection 8: Import bootstrap

--- header.php
+++ header.php
@@ -19,6 +19,10 @@
 </head>
 <body <?php body_class( 'hello-class' ); ?>>
 
-<?php wp_body_open(); ?>
+<?php 
+if ( function_exists( 'wp_body_open' )) {
+    wp_body_open(); 
+}
+?>
 
 <header>Header</header>
