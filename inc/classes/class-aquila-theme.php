6974d61ea8e73fb3c6737bcee0c029a7d66e72a8
Yang Wang <ywangwrs@gmail.com>
2021-02-12 08:08:59 -0500

    Until lecture 19: Add custom-logo
    Signed-off-by: Yang Wang <ywangwrs@gmail.com>

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -26,6 +26,14 @@ class AQUILA_THEME {
 
     public function setup_theme() {
         add_theme_support( 'title-tag' );
+
+        add_theme_support( 'custom-logo', [
+           'header-text' => array( 'site-title', 'site-description' ),
+           'height'      => 100,
+           'width'       => 400,
+           'flex-height' => true,
+           'flex-width'  => true,
+        ]);
     }
 
 }
