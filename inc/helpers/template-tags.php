186e796925935a0d2a8bd6a26d011c8b5440ef75
Yang Wang <ywangwrs@gmail.com>
2021-02-21 00:03:19 -0500

    Until lecture 39: Show posted date and hide modified date
    Signed-off-by: Yang Wang <ywangwrs@gmail.com>

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -16,7 +16,7 @@ function get_the_post_custom_thumbnail ( $post_id, $size = 'featured-thumbnail',
         $default_attributes = [
             'loading' => 'lazy'
         ];
-    
+
         $attributes = array_merge( $additional_attributes, $default_attributes );
 
         $custom_thumbnail = wp_get_attachment_image(
@@ -32,4 +32,28 @@ function get_the_post_custom_thumbnail ( $post_id, $size = 'featured-thumbnail',
 
 function the_post_custom_thumbnail ( $post_id, $size = 'featured-thumbnail', $additional_attributes = [] ) {
     echo get_the_post_custom_thumbnail( $post_id, $size, $additional_attributes );
+}
+
+function aquila_posted_on() {
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+    // Post is modified ( when post published time is not equal to the modified time )
+    // The modified time is hidden by style.css, the Entry meta part
+    if( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf( $time_string,
+        esc_attr( get_the_date( DATE_W3C ) ),
+        esc_attr( get_the_date() ),
+        esc_attr( get_the_modified_date( DATE_W3C ) ),
+        esc_attr( get_the_modified_date() )
+    );
+
+    $posted_on = sprintf(
+        esc_html_x( 'Posted on %s', 'post date', 'aquila' ),
+        '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+    );
+
+    echo '<span class="posted-one text-secondary">' . $posted_on . '</span>';
 }
