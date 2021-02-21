9a2c5bbf4c3e49a18a2f2e605b221d05b5a8126c
Yang Wang <ywangwrs@gmail.com>
2021-02-21 04:49:36 -0500

    Until lecture 41: Truncate post if it's too long
    Signed-off-by: Yang Wang <ywangwrs@gmail.com>

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,14 +1,16 @@
 <?php
 /**
  * Content template
- * 
+ *
  * @package aquila
  */
 
 ?>
 
+<!--
 <h4><?php the_title(); ?></h4>
 <div><?php the_excerpt(); ?></div>
+-->
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'mb-5' ); ?>>
     <?php
